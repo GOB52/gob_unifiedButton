8e09185b2fa29142d1016aaec9855b19a23d8c34
GOB <goblin52@gmail.com>
2024-03-16 13:53:50 +0900

    Fixes to use hort hash

--- doc/doxy.sh
+++ doc/doxy.sh
@@ -6,6 +6,6 @@
 ## Get git rev of HEAD
 LIB_VERSION="$(pcregrep -o1 "^\s*version\s*=\s*(\*|\d+(\.\d+){0,3}(\.\*)?)" library.properties)"
 #echo ${DOXYGEN_PROJECT_NUMBER}
-DOXYGEN_PROJECT_NUMBER="${LIB_VERSION} git rev:$(git rev-parse HEAD)" doxygen doc/Doxyfile
+DOXYGEN_PROJECT_NUMBER="${LIB_VERSION} git rev:$(git rev-parse --short HEAD)" doxygen doc/Doxyfile
 
 
